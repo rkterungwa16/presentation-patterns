401906eba642a31861bb57257de3667544133b42
rkterungwa16 <terungwakombol@gmail.com>
2019-07-24 13:40:12 +0100

    Experimenting with different ways to bind data to dom and changing this data on different events

--- mvc-slide-show/main.js
+++ mvc-slide-show/main.js
@@ -1,11 +1,10 @@
 
 define(function(require, exports, module) {
-    let slideIndex = 1;
-    const { createDots } = require("./dots");
-    const { showSlides, next, prev } = require("./slides");
-
-    createDots();
-    showSlides(slideIndex);
-    next();
-    prev();
+    const Slide = require("./slides");
+    const slide = new Slide();
+    slide.renderedSlides();
+    slide.dots();
+    slide.showSlides(1);
+    slide.next();
+    slide.prev();
 });
